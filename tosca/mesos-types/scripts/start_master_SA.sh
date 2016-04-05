86cf96340a9051d67daddd93e59df84e07daf491
FRAISSE ADRIAN <adrian.fraisse@fastconnect.fr>
2016-04-05 17:41:35 +0200

    Added stop operations

--- tosca/mesos-types/scripts/start_master_SA.sh
+++ tosca/mesos-types/scripts/start_master_SA.sh
@@ -5,6 +5,7 @@ echo "Starting mesos master..."
 source ~/mesos_install/mesos_env.txt
 sudo -E nohup mesos-master >~/start-master.log 2>&1 &
 
-ps -ef | grep -v grep >/dev/null | grep mesos-master >/dev/null || exit 1
+sleep 5
+ps -ef | grep -v grep | grep mesos-master >/dev/null || exit 1
 
 # Todo : vérifier que le master tourne (envoyer une tache de test ?)
