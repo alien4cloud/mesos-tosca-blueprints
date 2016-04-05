cc69f591675441989f87513942bd9f7895ea1434
FRAISSE ADRIAN <adrian.fraisse@fastconnect.fr>
2016-04-05 18:08:31 +0200

    Refactored

--- tosca/mesos-types/scripts/base/configure_host.sh
+++ tosca/mesos-types/scripts/base/configure_host.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/bash -e
 
 # Fix /etc/hosts
 sudo cp /etc/hosts /tmp/hosts
-echo ${IP} | sudo tee /etc/hosts >/dev/null
-cat  /tmp/hosts | sudo tee -a /etc/hosts > /dev/null
+echo "${IP}" `hostname` | sudo tee /etc/hosts >/dev/null
+cat /tmp/hosts | sudo tee -a /etc/hosts >/dev/null
