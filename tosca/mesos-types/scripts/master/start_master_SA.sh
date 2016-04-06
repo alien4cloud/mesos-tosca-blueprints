66fa11d495614822e915c9ea20ce187e5ed0af3e
FRAISSE ADRIAN <adrian.fraisse@fastconnect.fr>
2016-04-06 16:42:57 +0200

    Fix syntax issues

--- tosca/mesos-types/scripts/master/start_master_SA.sh
+++ tosca/mesos-types/scripts/master/start_master_SA.sh
@@ -3,7 +3,7 @@
 # Starts a mesos Master in stand-alone mode
 echo "Starting mesos master..."
 source ~/mesos_install/mesos_env.txt
-sudo -E nohup mesos-master >~/start-master.log 2>&1 &
+sudo -E nohup mesos-master >/dev/null 2>&1 &
 
 sleep 5
 ps -ef | grep -v grep | grep mesos-master >/dev/null || exit 1
--- tosca/mesos-types/scripts/slave/start_slave.sh
+++ tosca/mesos-types/scripts/slave/start_slave.sh
@@ -2,9 +2,9 @@
 
 # Starts a mesos slave
 echo "Starting mesos slave..."
-echo "export MESOS_MASTER=${MESOS_MASTER}" >> ~/mesos_install/mesos_env.txt
+echo "export MESOS_MASTER=\"${MESOS_MASTER}\"" >> ~/mesos_install/mesos_env.txt
 source ~/mesos_install/mesos_env.txt
-sudo -E nohup mesos-slave >~/start-slave.log 2>&1 &
+sudo -E nohup mesos-slave >/dev/null 2>&1 &
 
 sleep 5
 if [ $? = 0 ]; then
