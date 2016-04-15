42dd603e02d57fc7cd421a62da5f434235af9e23
FRAISSE ADRIAN <adrian.fraisse@fastconnect.fr>
2016-04-15 15:21:57 +0200

    Changed while loop into for loop and added quorum parameter

--- tosca/mesos-types/scripts/master/start.sh
+++ tosca/mesos-types/scripts/master/start.sh
@@ -6,21 +6,25 @@ sudo service zookeeper restart
 
 # Put together the zk url
 zkURL="zk://"
-while IFS=',' read inst; do
-    host=eval "echo \$$inst$IP"
-    echo "... ${inst} : ${IP}"
-    zkURL="$zkURL$host:2181,"
-done < <(echo $INSTANCES)
-# Remove the last comma and add mesos path
+IFS=','
+for inst in $INSTANCES
+do
+    host="${inst}_IP"
+    zkURL="$zkURL${!host}:2181,"
+done
 zkURL="${zkURL%?}/mesos"
+# Quorum for replicated log must be a majority of masters
+quorum=$((1 + $(echo $INSTANCES | tr "," " " | wc -w)/2))
 
 # Starts a mesos Master
 echo "Starting mesos master..."
 source ~/mesos_install/mesos_env.sh
-sudo -E nohup mesos-master --zk=${zkURL} >/dev/null 2>&1 &
+
+echo "sudo -E nohup mesos-master --zk=${zkURL} --quorum=${quorum} >/dev/null 2>~/mesos_install/err.log &"
+sudo -E nohup mesos-master --zk=${zkURL} --quorum=${quorum} >/dev/null 2>~/mesos_install/err.log &
 
 sleep 5
 
-# TODO : check if standalone mode and return the IP address
+# TODO : check if standalone mode and return the IP address - is it necessary considering ZK works fine in SAM ?
 export master_url=${zkURL}
 ps -ef | grep -v grep | grep mesos-master >/dev/null || exit 1
