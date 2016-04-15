08db8bc0ff876ff18d7f7f2f68697545d6d6aaff
FRAISSE ADRIAN <adrian.fraisse@fastconnect.fr>
2016-04-15 09:51:51 +0200

    Removed MesosBase, renamed scripts and added zookeeper integration to the MesosMaster component

--- tosca/mesos-types/scripts/base/configure_host.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-
-# Fix /etc/hosts
-sudo cp /etc/hosts /tmp/hosts
-echo "${IP}" `hostname` | sudo tee /etc/hosts >/dev/null
-cat /tmp/hosts | sudo tee -a /etc/hosts >/dev/null
---
+++ tosca/mesos-types/scripts/master/start.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+
+# Starts zookeeper
+# sudo -E nohup bin/zkserver.sh >/dev/null 2>&1 &
+sudo service zookeeper restart
+
+# Put together the zk url
+zkURL="zk://"
+while IFS=',' read inst; do
+    host=eval "echo \$$inst$IP"
+    echo "... ${inst} : ${IP}"
+    zkURL="$zkURL$host:2181,"
+done < <(echo $INSTANCES)
+# Remove the last comma and add mesos path
+zkURL="${zkURL%?}/mesos"
+
+# Starts a mesos Master
+echo "Starting mesos master..."
+source ~/mesos_install/mesos_env.txt
+sudo -E nohup mesos-master --zk=${zkURL} >/dev/null 2>&1 &
+
+sleep 5
+
+# TODO : check if standalone mode and return the IP address
+export master_url=${zkURL}
+ps -ef | grep -v grep | grep mesos-master >/dev/null || exit 1
