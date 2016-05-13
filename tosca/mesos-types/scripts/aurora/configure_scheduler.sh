a4a101ad31f16393941d05f067cdb0290e001265
FRAISSE ADRIAN <adrian.fraisse@fastconnect.fr>
2016-05-13 14:23:48 +0200

    Set the ZK ensemble for tasks announcement & service discovery

--- tosca/mesos-types/scripts/aurora/configure_scheduler.sh
+++ tosca/mesos-types/scripts/aurora/configure_scheduler.sh
@@ -18,5 +18,9 @@ sudo sed -i.bak "s/^\(QUORUM_SIZE=\)[0-9]*$/\1${quorum}/" /etc/default/aurora-sc
 # Use the known compute hostname to advertise in ZooKeeper instead of the locally-resolved hostname.
 sudo sed -i.bak "s/^\(EXTRA_SCHEDULER_ARGS=\).*$/\1\"-hostname=\\\\\"${HOSTNAME}\\\\\"\"/" /etc/default/aurora-scheduler
 
+zk_ensemble=$(echo $AURORA_ZK_ENDPOINTS | tr " " ",")
+# Set the ZK ensemble that tasks will use for service discovery. TODO : use a different ZK ensemble
+sudo sed -i.bak "s/^\(THERMOS_EXECUTOR_FLAGS=\"\).*$/\1--announcer-ensemble ${zk_ensemble}\"/" /etc/default/aurora-scheduler
+
 # Remove backup file
 sudo rm -f /etc/default/aurora-scheduler.bak
