955f0b449ca3bffa86e3f959af7f8f1cb7543ec4
FRAISSE ADRIAN <adrian.fraisse@fastconnect.fr>
2016-05-23 15:36:38 +0200

    Extend timeout for executor registration when using docker containerization

--- tosca/mesos-types/scripts/slave/configure_dependsOnDocker.sh
+++ tosca/mesos-types/scripts/slave/configure_dependsOnDocker.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # Add the docker containerizer to Mesos slave configuration
-echo "export MESOS_CONTAINERIZERS=\"mesos,docker\"" >> ~/mesos_install/mesos_env.sh
+echo "export MESOS_CONTAINERIZERS=\"docker,mesos\"" >> ~/mesos_install/mesos_env.sh
+# Enlarge executor registration timeout to grant docker enough time to pull images
+echo "export MESOS_EXECUTOR_REGISTRATION_TIMEOUT=\"5mins\"" >> ~/mesos_install/mesos_env.sh
