bcee0dc2d677987e7157c5a0604fbd15f9811500
FRAISSE ADRIAN <adrian.fraisse@fastconnect.fr>
2016-06-23 11:50:56 +0200

    Added support for Mesos-DNS
    Following the installation of Marathon, Mesos-DNS packages are pull on each slave node.
    A sample mesos-dns config.json file is issued and configured with the proper ZK address and resolvers.
    
    Mesos-DNS should afterward be run with Marathon on one or more slaves. The /etc/resolv.conf file must be updated on each slave accordingly.To allow service discovery using Marathon-lb, we also added reserved ports in the slaves' configuration.
    
    NOTE: This is WIP and is actually designed for a POC. In the future, we should distinguish slaves set up with service discovery features from the other slaves in the cluster.

--- tosca/mesos-types/scripts/slave/configure.sh
+++ tosca/mesos-types/scripts/slave/configure.sh
@@ -8,3 +8,23 @@ while IFS='=' read name value; do
     echo "export ${name}=\"${value}\"" >> ~/mesos_install/mesos_env.sh
   fi
 done < <(env | grep "^MESOS_")
+
+#TODO : add ports resources for LB --resources=ports:[9090-9091,10000-101000,31000-32000]
+echo "export MESOS_RESOURCES=\"ports:[53,8123,9090-9091,10000-101000,31000-32000]\"" >> ~/mesos_install/mesos_env.sh
+
+# TODO move this into a standalone component
+# setup mesos-dns
+sudo mkdir /usr/local/mesos-dns/
+sudo wget -O /usr/local/mesos-dns/mesos-dns https://github.com/mesosphere/mesos-dns/releases/download/v0.5.1/mesos-dns-v0.5.1-linux-amd64
+sudo chmod +x mesos-dns
+sudo cp ${dns_config} /usr/local/mesos-dns/config.json
+
+# setup resolvers
+declare -a nameservers=($(cat /etc/resolv.conf | grep '^nameserver' | awk '{print $2}'))
+resolvers=""
+for res in "${nameservers[@]}"
+do
+  resolvers="${resolvers}\"${res}\","
+done
+resolvers="${resolvers%?}"
+sudo sed -i "s/{{resolvers}}/${resolvers}/" /usr/local/mesos-dns/config.json
