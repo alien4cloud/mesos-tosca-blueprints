af803d5bb7b140e876a45774e4a25c0c410d504e
FRAISSE ADRIAN <adrian.fraisse@fastconnect.fr>
2016-07-06 16:24:15 +0200

    Use private_ip for DNS resolution

--- tosca/mesos-types/scripts/service-discovery/configure-mesosdns.sh
+++ tosca/mesos-types/scripts/service-discovery/configure-mesosdns.sh
@@ -12,7 +12,7 @@ resolvers="${resolvers%?}"
 sudo sed -i "s/{{resolvers}}/${resolvers}/" /usr/local/mesos-dns/config.json
 
 # Add self as a dns server
-sudo sed -i "1s/^/nameserver ${SLAVE_IP}\n /" /etc/resolvconf/resolv.conf.d/head
+sudo sed -i "1s/^/nameserver ${SLAVE_LOCAL_IP}\n /" /etc/resolvconf/resolv.conf.d/head
 sudo resolvconf -u
 
 # Configure marathon application file
