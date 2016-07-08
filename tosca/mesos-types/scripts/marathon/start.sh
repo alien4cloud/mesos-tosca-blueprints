e29b712bf08ee0815262712962871061c899eec6
FRAISSE ADRIAN <adrian.fraisse@fastconnect.fr>
2016-07-08 11:14:53 +0200

    Set --access_control_allow_origin to *

--- tosca/mesos-types/scripts/marathon/start.sh
+++ tosca/mesos-types/scripts/marathon/start.sh
@@ -1,10 +1,9 @@
 #!/bin/bash -e
-
 # Starting marathon as sudo while retaining environment variables
-# TODO: Workaround : cfy replaces commas with whitespaces
-export MARATHON_MASTER=$(echo $MARATHON_MASTER | tr " " ",")
-export MARATHON_ZK=$(echo $MARATHON_ZK | tr " " ",")
-sudo -E nohup marathon 0</dev/null &>/dev/null &
+# NOTE: Workaround - cfy replaces commas with whitespaces
+master=$(echo $MARATHON_MASTER | tr " " ",")
+zk=$(echo $MARATHON_ZK | tr " " ",")
+sudo -E nohup marathon --master=$master --zk=$zk --access_control_allow_origin="*" 0</dev/null &>/dev/null &
 sleep 5
 
 ps -ef | grep -v grep | grep marathon >/dev/null || (echo "Failed to start marathon"; exit 1)
