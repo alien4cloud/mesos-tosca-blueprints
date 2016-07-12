44e99f4ee8342688758b37581e974f0fbb76a1e3
FRAISSE ADRIAN <adrian.fraisse@fastconnect.fr>
2016-07-12 17:53:38 +0200

    Fixed issue when starting marathon

--- tosca/mesos-types/scripts/marathon/start.sh
+++ tosca/mesos-types/scripts/marathon/start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 # Starting marathon as sudo while retaining environment variables
 # NOTE: Workaround - cfy replaces commas with whitespaces
-master=$(echo $MARATHON_MASTER | tr " " ",")
-zk=$(echo $MARATHON_ZK | tr " " ",")
-sudo -E nohup marathon --master=$master --zk=$zk --access_control_allow_origin="*" 0</dev/null &>/dev/null &
+MARATHON_MASTER=$(echo $MARATHON_MASTER | tr " " ",")
+MARATHON_ZK=$(echo $MARATHON_ZK | tr " " ",")
+sudo -E nohup marathon --access_control_allow_origin="*" 0</dev/null &>/dev/null &
 sleep 5
 
 ps -ef | grep -v grep | grep marathon >/dev/null || (echo "Failed to start marathon"; exit 1)
