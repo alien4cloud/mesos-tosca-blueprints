28c82049998be020c2c15935e469a228853c87ce
afraisse <adrian.fraisse@gmail.com>
2016-11-15 19:50:45 +0100

    Use curl instead of wget for portability

--- mesos-types/scripts/service-discovery/install-mesosdns.sh
+++ mesos-types/scripts/service-discovery/install-mesosdns.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 # setup mesos-dns
 sudo mkdir /usr/local/mesos-dns/
-sudo wget -O /usr/local/mesos-dns/mesos-dns https://github.com/mesosphere/mesos-dns/releases/download/v0.5.1/mesos-dns-v0.5.1-linux-amd64
+sudo curl -sSL -o /usr/local/mesos-dns/mesos-dns https://github.com/mesosphere/mesos-dns/releases/download/v0.5.1/mesos-dns-v0.5.1-linux-amd64
 sudo chmod +x /usr/local/mesos-dns/mesos-dns
